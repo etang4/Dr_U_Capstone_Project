e4866cbc961bcccecf8eea2a18f2282e71a31d5f
REALDrummer <conman.davey@gmail.com>
2016-01-29 17:32:19 -0700

    I started implementing input smoothing.
    It seems that the A.P.I. works quite differently from the one I implemented in the code, so I will have to make some significant structural changes to the algorithm. It should work the same way, though. We'll see!

---
+++ Dr_U_Unity_Project/Assets/Scripts/InputSmoother.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InputSmoother : MonoBehaviour {
+	private static SortedDictionary<iBeaconReceiver, InputSmoother> smoothers = new SortedDictionary<>();
+	private static 
+
+	// when the program starts or ends
+	void Start() {
+		iBeaconReceiver.BeaconRangeChangedEvent += TryUpdateBeaconRanges;
+		iBeaconReceiver.BluetoothStateChangedEvent += TrackBluetoothStateChanges;
+		iBeaconReceiver.CheckBluetoothLEStatus();
+		Debug.Log("listening for beacons...");
+	}
+	
+	void OnDestroy() {
+		iBeaconReceiver.BeaconRangeChangedEvent -= TryUpdateBeaconRanges;
+		iBeaconReceiver.BluetoothStateChangedEvent -= TrackBluetoothStateChanges;
+	}
+	
+	// Bluetooth event handling
+	private void TryUpdateBeaconRanges(List<Beacon> beacons) {
+		
+	}
+}
