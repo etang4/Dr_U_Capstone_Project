30b2bff0cb14a42005bb51592fcfef465711153e
REALDrummer <conman.davey@gmail.com>
2016-02-03 21:03:00 -0700

    Most integration done!
    Most of the integration of this project into Unity with the iBeacon module is done. There are a few compilation errors and there is testing yet to be done.

--- Dr_U_Unity_Project/Assets/Scripts/InputSmoother.cs
+++ Dr_U_Unity_Project/Assets/Scripts/InputSmoother.cs
@@ -1,26 +1,62 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
-public class InputSmoother : MonoBehaviour {
-	private static SortedDictionary<iBeaconReceiver, InputSmoother> smoothers = new SortedDictionary<>();
-	private static 
+public class InputSmoother {
+	private const int DISCONNECTION_THRESHOLD = 5;
 
-	// when the program starts or ends
-	void Start() {
-		iBeaconReceiver.BeaconRangeChangedEvent += TryUpdateBeaconRanges;
-		iBeaconReceiver.BluetoothStateChangedEvent += TrackBluetoothStateChanges;
-		iBeaconReceiver.CheckBluetoothLEStatus();
-		Debug.Log("listening for beacons...");
-	}
+	private Beacon beacon;
+	private Queue<uint> window = new Queue<uint>();
+	private uint window_size;
+	private uint current_sum = 0;
+	private bool is_connected = true;
 	
-	void OnDestroy() {
-		iBeaconReceiver.BeaconRangeChangedEvent -= TryUpdateBeaconRanges;
-		iBeaconReceiver.BluetoothStateChangedEvent -= TrackBluetoothStateChanges;
-	}
-	
-	// Bluetooth event handling
-	private void TryUpdateBeaconRanges(List<Beacon> beacons) {
+	public InputSmoother(Beacon beacon, uint window_size)
+	{
+		this.beacon = beacon;
+		this.window_size = window_size;
 		
+		Update();
+	}
+	
+	public void Update()
+	{
+		// TODO TEMP
+		Debug.Log("[" + i + "] direct strength = " + beacon.strength.ToString());
+		
+		// add the new signal strength value to the window
+		window.Enqueue(beacon.strength);
+		current_sum += beacon.strength;
+		// remove the old signal strength value from the window if needed
+		if (window.Count > window_size)
+		{
+			current_sum -= window.Dequeue();
+			// if the window is full and connection is very weak, discard the connection
+			if (GetSignalStrength() < DISCONNECTION_THRESHOLD)
+			{
+				is_connected = false;
+			}
+		}
+		
+		// TODO TEMP
+		Debug.Log("[" + i + "] smoothed strength = " + GetSignalStrength().ToString() + " (size=" + window.Count + ")");
+	}
+
+	public InputSmoother(Beacon beacon)
+	{
+		this.beacon = beacon;
+	}
+	
+	public Beacon GetBeacon()
+	{
+		return beacon;
+	}
+	
+	public bool IsConnected()
+	{
+		return is_connected;
+	}
+	
+	public uint GetSignalStrength()
+	{
+		return current_sum / window.Count;
 	}
 }
---
+++ Dr_U_Unity_Project/Assets/Scripts/InputSmootherHelper.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+
+public class InputSmootherHelper : MonoBehaviour {
+	private static Set<InputSmoother> smoothers = new HashSet<InputSmoother>();
+	private static Set<Beacon> beacons = new HashSet<Beacon>();
+	private static Timer update_timer = new Timer(100);
+	
+	// when the program starts or ends
+	void Start()
+	{
+		iBeaconReceiver.BeaconRangeChangedEvent += TrackNewBeaconConnections;
+		iBeaconReceiver.BluetoothStateChangedEvent += TrackBluetoothStateChanges;
+		iBeaconReceiver.CheckBluetoothLEStatus();
+		
+		update_timer.Elapsed += UpdateBeaconRanges;
+		update_timer.AutoReset = true;
+		update_timer.Enabled = true;
+		
+		Debug.Log("listening for beacons...");
+	}
+	
+	void OnDestroy()
+	{
+		iBeaconReceiver.BeaconRangeChangedEvent -= TrackNewBeaconConnections;
+		iBeaconReceiver.BluetoothStateChangedEvent -= TrackBluetoothStateChanges;
+	}
+	
+	// Bluetooth event handling
+	private static void UpdateBeaconRanges(Object source, ElapsedEventArgs e)
+	{
+		Set<InputSmoother> disconnected_smoothers = new HashSet<InputSmoother>();
+		foreach (InputSmoother smoother in smoothers)
+		{
+			smoother.Update();
+			if (!smoother.IsConnected())
+			{
+				disconnected_smoothers.Add(smoother);
+			}
+		}
+		
+		// remove disconnected smoothers
+		foreach (InputSmoother disconnected_smoother in disconnected_smoothers)
+		{
+			beacons.Remove(disconnected_smoother.GetBeacon());
+			smoothers.Remove(disconnected_smoother);
+		}
+	}
+	
+	private static void TrackNewBeaconConnections(List<Beacon> updated_beacons)
+	{
+		foreach (Beacon updated_beacon in updated_beacons)
+		{
+			if (!beacons.Contains(updated_beacon))
+			{
+				beacons.Add(updated_beacon);
+				smoothers.Add(new InputSmoother(updated_beacon));
+			}
+		}
+	}
+	
+	private static void TrackBluetoothStateChanges(BluetoothLowEnergyState newstate)
+	{
+		switch (newstate)
+		{
+		case BluetoothLowEnergyState.POWERED_ON:
+			iBeaconReceiver.Init();
+			update_timer.Enabled = true;
+			Debug.Log ("Bluetooth is on!");
+			break;
+		case BluetoothLowEnergyState.POWERED_OFF:
+			update_timer.Enabled = false;
+			iBeaconReceiver.EnableBluetooth();
+			Debug.Log ("Bluetooth turned off; I'll try to turn it back on.");
+			break;
+		case BluetoothLowEnergyState.UNAUTHORIZED:
+			Debug.Log("The user doesn't want this app to use Bluetooth!");
+			break;
+		case BluetoothLowEnergyState.UNSUPPORTED:
+			Debug.Log ("This device doesn't support Bluetooth Low Energy!");
+			break;
+		default:
+			Debug.Log ("I don't know what this \"" + newstate + "\" Bluetooth state means!");
+			break;
+		}
+	}
+}
