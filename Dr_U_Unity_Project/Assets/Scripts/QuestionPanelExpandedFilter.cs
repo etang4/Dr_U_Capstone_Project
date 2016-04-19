c4f60d82fb2a7df5ee4a0d667b9aa35cfacf0ad8
Michael Sprague <odingrey@gmail.com>
2016-04-19 16:12:22 -0700

    Merged with Patrick Local

--- Dr_U_Unity_Project/Assets/Scripts/QuestionPanelExpandedFilter.cs
+++ Dr_U_Unity_Project/Assets/Scripts/QuestionPanelExpandedFilter.cs
@@ -4,6 +4,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
+
+/*
+    This class handles the search results and dynamically generates
+    result buttons.
+*/
 public class QuestionPanelExpandedFilter : MonoBehaviour
 {
 	public GameObject originalButton;
@@ -23,8 +28,6 @@ public class QuestionPanelExpandedFilter : MonoBehaviour
 		_isInstantiated = false;
 		filterList(SearchBarText.text);         //Initial run
 		_isInstantiated = true;
-		
-		
 	}
 	
 	
@@ -34,6 +37,7 @@ public class QuestionPanelExpandedFilter : MonoBehaviour
 		Dictionary<string, string> searchResults = fakeSearch(result, 1);          //Update this to use real data
 		
 		int question = 0;
+        // Destroy old items
 		foreach(GameObject item in itemsList){
 			Destroy(item);
 		}
@@ -41,25 +45,28 @@ public class QuestionPanelExpandedFilter : MonoBehaviour
 		listSize = searchResults.Count;
 		
 		//TODO: Cannot test if this works until search is implemented
-		if (!_isInstantiated)
-		{
-			faqGrid = containerRect.GetComponent<GridLayoutGroup>();
-			faqRect = containerRect.GetComponent<RectTransform>();
-			faqGrid.cellSize = new Vector2(faqRect.rect.width, faqRect.rect.height / 7);
-			faqRect.sizeDelta = new Vector2(faqRect.sizeDelta.x, (faqGrid.cellSize.y + faqGrid.spacing.y) * (listSize - 4) - faqGrid.spacing.y * 4);
-			// +4 does not work for large data sets, this needs to be reconfigured
-			
-			faqRect.offsetMax = new Vector2(containerRect.GetComponent<RectTransform>().offsetMax.x, 0);
-		}
+        if (!_isInstantiated)
+        {
+            faqGrid = containerRect.GetComponent<GridLayoutGroup>();
+            faqRect = containerRect.GetComponent<RectTransform>();
+            faqGrid.cellSize = new Vector2(faqRect.rect.width, faqRect.rect.height / 7);
+            faqRect.sizeDelta = new Vector2(faqRect.sizeDelta.x, (faqGrid.cellSize.y + faqGrid.spacing.y) * (listSize - 4) - faqGrid.spacing.y * 4);
+            // +4 does not work for large data sets, this needs to be reconfigured
+            //Adjusting area of where results can be placed.
+            faqRect.offsetMax = new Vector2(containerRect.GetComponent<RectTransform>().offsetMax.x, 0);
+        }
 		
 		foreach (KeyValuePair<string, string> answer in searchResults)
 		{
 			GameObject newButton = Instantiate(originalButton);
 			itemsList[question] = newButton;
 			FAQButton FAQ = newButton.GetComponent<FAQButton>();
-			FAQ.question = answer.Key;
-			FAQ.answer = answer.Value;
-			newButton.transform.GetChild(0).GetComponent<Text>().text = FAQ.question;
+            QuestionAnswerPair newQAPair = new QuestionAnswerPair();
+            // Assign spanish or english answers here
+            newQAPair.question = answer.Key;
+            newQAPair.answer = answer.Value;
+            FAQ.faqPair = newQAPair;
+            newButton.transform.GetChild(0).GetComponent<Text>().text = newQAPair.question;
 			
 			newButton.transform.parent = this.transform;
 			question++;
@@ -67,9 +74,9 @@ public class QuestionPanelExpandedFilter : MonoBehaviour
 		
 		ResourceCounter.addQuestionsAsked(1,question);	// If question = 0, then the search term returned no results, and thus Dr. U was stumped.
 	}
-	
-	
-	
+
+
+    //TODO: Fake Search, remove when Lucene Search is implemented
 	private Dictionary<string, string> fakeSearch(string input, int id)
 	{
 		Dictionary<string, string> dict = new Dictionary<string, string>();
